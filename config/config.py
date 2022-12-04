06200ce39e341597e1f9c877450ef79999170f56
Krishna <119519282+RocksKrishnaBot@users.noreply.github.com>
2022-12-04 13:20:48 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -36,7 +36,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Team-Alisha/AnonX",
+    "https://github.com/RocksKrishnaBot/OpKrishnaMusiC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
